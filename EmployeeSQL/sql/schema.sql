91586b57a0e8ca3ab824af54f102b1aefaa35ca2
Stuart Yates <sjwyates@Stuarts-MacBook-Pro.local>
2020-11-14 17:44:32 -0700

    set up schema

---
+++ EmployeeSQL/sql/schema.sql
@@ -0,0 +1,40 @@
+DROP TABLE IF EXISTS employees;
+CREATE TABLE employees (
+	emp_no VARCHAR NOT NULL PRIMARY KEY,
+	emp_title VARCHAR NOT NULL,
+	birth_date VARCHAR NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	sex VARCHAR NOT NULL,
+	hire_date VARCHAR NOT NULL
+);
+
+DROP TABLE IF EXISTS departments;
+CREATE TABLE departments (
+	dept_no VARCHAR NOT NULL PRIMARY KEY,
+	dept_name VARCHAR NOT NULL
+);
+
+DROP TABLE IF EXISTS salaries;
+CREATE TABLE salaries (
+	emp_no VARCHAR NOT NULL,
+	salary INTEGER NOT NULL
+);
+
+DROP TABLE IF EXISTS titles;
+CREATE TABLE titles (
+	title_id VARCHAR NOT NULL PRIMARY KEY,
+	title VARCHAR NOT NULL
+);
+
+DROP TABLE IF EXISTS dept_emp;
+CREATE TABLE dept_emp (
+	emp_no VARCHAR NOT NULL,
+	dept_no VARCHAR NOT NULL
+);
+
+DROP TABLE IF EXISTS dept_manager;
+CREATE TABLE dept_manager (
+	dept_no VARCHAR NOT NULL,
+	emp_no VARCHAR NOT NULL
+);
