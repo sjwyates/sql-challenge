43d69076d38026cd2e644870f27d59328d2c6c87
Stuart Yates <sjwyates@Stuarts-MacBook-Pro.local>
2020-11-14 18:00:37 -0700

    department managers

--- EmployeeSQL/sql/query.sql
+++ EmployeeSQL/sql/query.sql
@@ -6,7 +6,7 @@ SELECT
 	s.salary
 FROM employees AS e
 JOIN salaries AS s
-ON e.emp_no = s.emp_no;
+  ON e.emp_no = s.emp_no;
 
 SELECT
 	first_name,
@@ -15,3 +15,15 @@ SELECT
 FROM employees
 WHERE EXTRACT(YEAR FROM CAST(hire_date AS DATE)) = 1986;
 
+SELECT
+	d.dept_no,
+	d.dept_name,
+	e.emp_no,
+	e.first_name,
+	e.last_name
+FROM departments AS d
+JOIN dept_manager AS dm
+  ON d.dept_no = dm.dept_no
+JOIN employees AS e
+  ON dm.emp_no = e.emp_no;
+	
